e022b484b8cb0b0419d1df9a60c41c8ad3fcb227
Parkjieun <0224h@naver.com>
2024-09-25 02:59:08 +0900

    feat: 메뉴바 버튼 스타일 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { CONTAINER_WIDTH, HEADER_HEIGHT } from "../utils/layouts";
 import { Typography, Button } from "antd";
 import LogoIcon from "../assets/images/logo.png";
 import { BLUE, GRAY } from "../utils/colors";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const [selectedMenu, setSelectedMenu] = useState("DB 조회");
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/userlist") {
+      setSelectedMenu("DB 조회");
+    } else if (location.pathname === "/setting") {
+      setSelectedMenu("Settings");
+    }
+  }, [location.pathname]);
+
   return (
     <Container>
       <ItemContainer>
@@ -18,14 +30,22 @@ const Header = () => {
       </ItemContainer>
       <MenuContainer>
         <Link to="/userlist" style={{ textDecoration: "none" }}>
-          <Menu>DB 조회</Menu>
+          <Menu
+            onClick={() => setSelectedMenu("DB 조회")}
+            selected={selectedMenu === "DB 조회"}
+          >
+            DB 조회
+          </Menu>
         </Link>
         <Link to="/setting" style={{ textDecoration: "none" }}>
-          <Menu>Settings</Menu>
+          <Menu
+            onClick={() => setSelectedMenu("Settings")}
+            selected={selectedMenu === "Settings"}
+          >
+            Settings
+          </Menu>
         </Link>
-        <Menu style={{ backgroundColor: BLUE.DEFAULT, color: "white" }}>
-          로그아웃
-        </Menu>
+        <LogoutButton>로그아웃</LogoutButton>
       </MenuContainer>
     </Container>
   );
@@ -77,6 +97,26 @@ const MenuContainer = styled.div`
 
 const Menu = styled(Button)`
   margin-left: 20px;
+  border: 1.5px solid;
+  color: ${({ selected }) => (selected ? BLUE.DARK : GRAY.DARK)};
+
+  border-color: ${({ selected }) => (selected ? BLUE.DARK : GRAY.DEFAULT)};
+
+  &:hover {
+    color: ${BLUE.DEFAULT};
+    border-color: ${BLUE.DEFAULT};
+  }
+`;
+
+const LogoutButton = styled(Button)`
+  margin-left: 20px;
+  background-color: ${BLUE.DEFAULT};
+  color: white;
+
+  &:hover {
+    background-color: ${GRAY.DEFAULT} !important;
+    color: white !important;
+  }
 `;
 
 const BuddTypo = styled(Typography)`
